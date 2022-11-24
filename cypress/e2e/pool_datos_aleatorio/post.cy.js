5fe53fd42974471a63011f4cbeec95604d53f8a6
jcamilorg <juancamilorg64@gmail.com>
2022-11-23 22:34:20 -0500

    prueba post random

--- cypress/e2e/pool_datos_aleatorio/post.cy.js
+++ cypress/e2e/pool_datos_aleatorio/post.cy.js
@@ -1,12 +1,15 @@
 import { faker } from "@faker-js/faker";
 
 describe("Admin create new post", (feature = "post") => {
-  it("Como usuario administrador creo un post con un titulo y descripción", () => {
-    const title = faker.name.jobTitle();
-    const description = faker.lorem.paragraph();
+  beforeEach(() => {
     cy.intercept("/ghost/api/admin/settings").as("saveSettings");
     cy.goAdminAndLogin();
     cy.wait(3000);
+  });
+
+  it("Como usuario administrador creo un post con un titulo y descripción con datos normales", () => {
+    const title = faker.name.jobTitle();
+    const description = faker.lorem.paragraph();
     cy.visit("http://uniandes.ingenio.com.co:2368/ghost/#/posts/");
     cy.wait(3000);
     cy.screenshot(`images/cypress/${feature}/navigate_post`);
@@ -31,12 +34,9 @@ describe("Admin create new post", (feature = "post") => {
     cy.get("div.single-content p").should("have.text", description);
   });
 
-  it("Como usuario administrador edito un post publicado", (feature = "post") => {
+  it("Como usuario administrador edito un post publicado con datos normales", (feature = "post") => {
     const title = faker.name.jobTitle();
     const description = faker.lorem.paragraph();
-    cy.intercept("/ghost/api/admin/settings").as("saveSettings");
-    cy.goAdminAndLogin();
-    cy.wait(3000);
     cy.visit(
       "http://uniandes.ingenio.com.co:2368/ghost/#/posts?type=published"
     );
@@ -58,4 +58,23 @@ describe("Admin create new post", (feature = "post") => {
     cy.get("h1.single-title").should("have.text", title);
     cy.get("div.single-content p").should("have.text", description);
   });
+
+  it.only("Como usuario administrador creo un post con un titulo con un titulo mas de 100 palabras", () => {
+    const title = faker.lorem.sentence(100);
+    const description = faker.lorem.paragraph(10);
+
+    cy.visit("http://uniandes.ingenio.com.co:2368/ghost/#/posts/");
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}/navigate_post`);
+    cy.get("a.ember-view.gh-btn.gh-btn-primary.view-actions-top-row")
+      .contains("New post")
+      .click();
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}/create_post`);
+    cy.get("textarea.gh-editor-title").clear().type(title);
+    cy.get("article.koenig-editor").clear().type(description);
+    cy.get("button.gh-publish-trigger").should("not.exist");
+  });
+
+  // Titulo largo y titulo corto y titulo largo verifcar error que permite publicar
 });
